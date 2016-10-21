99de46466b302492f2b86a4833983a795ac02261
looiic <loic.jermann@gibmit.ch>
2016-10-21 10:28:20 +0200

    1

--- app/src/main/java/com/example/loicjermann/schatzkarte/MainActivity.java
+++ app/src/main/java/com/example/loicjermann/schatzkarte/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.loicjermann.schatzkarte;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.MapTileProviderBase;
+import org.osmdroid.tileprovider.modules.IArchiveFile;
+import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
+
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map = (MapView) findViewById(R.id.mapview);
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+
+        map.setMultiTouchControls(true);
+        map.setBuiltInZoomControls(true);
+
+        IMapController controller = map.getController();
+        controller.setZoom(18);
+
+        XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", 1, 20, 256, ".png",new String[] {"http://example.org/"});
+
+        File file = new File(Environment.getExternalStorageDirectory() /* entspricht /sdcard/ */, "hsr.mbtiles");
+
+        MapTileModuleProviderBase treasureMapModuleProvider = new MapTileFileArchiveProvider(new SimpleRegisterReceiver(this),
+                treasureMapTileSource, new IArchiveFile[] { MBTilesFileArchive.getDatabaseFileArchive(file) });
+
+        MapTileProviderBase treasureMapProvider = new MapTileProviderArray(treasureMapTileSource, null,
+                new MapTileModuleProviderBase[] { treasureMapModuleProvider });
+
+        TilesOverlay treasureMapTilesOverlay = new TilesOverlay(treasureMapProvider, getBaseContext());
+        treasureMapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
     }
 }
